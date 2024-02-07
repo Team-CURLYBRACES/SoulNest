486b3df0453ff924acfa3fb9480b9f548879c236
amethaaa <amethajayawardena@gmail.com>
2024-02-08 03:54:38 +0530

    Added signup page

--- lib/presentation/screens/sign_up_screen/sign_up_screen.dart
+++ lib/presentation/screens/sign_up_screen/sign_up_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/custom_button.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/divider.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/forget_password_checkbox.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/input_filed.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/sign_in_logos.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/sign_up.dart';
+import 'package:soulnest/presentation/screens/login_screen/widgets/title_text.dart';
+import 'package:soulnest/presentation/screens/sign_up_screen/widgets/infoarea.dart';
 
 void main() {
   runApp(const SignUpScreen());
@@ -10,10 +18,69 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sign-Up"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Expanded(
+              child: SizedBox(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Image(
+                      image: AssetImage("assets/logos/soulNest.png"),
+                      width: 100,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InfoArea(),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InputFiled(
+                      textFiledTitle: "Full Name",
+                      inputText: "John Doe",
+                      showText: false,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    InputFiled(
+                      textFiledTitle: "E-mail",
+                      inputText: "john.doe@gmail.com",
+                      showText: true,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                   
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomeButton(buttonText: "Next"),
+                    SizedBox(
+                      height: 10,
+                    ),
+
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                    SizedBox(
+                      height: 20,
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: const Center(child: Text("Sign Up Screen")),
     );
   }
 }
---
+++ lib/presentation/screens/sign_up_screen/widgets/infoarea.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class InfoArea extends StatelessWidget {
+  const InfoArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Text(
+                "Hey there!",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18
+              ),
+            ),
+            Icon(
+              Icons.waving_hand_rounded
+            )
+          ],
+        ),
+        Text("We are excited to have you on board. Let's get to know each other better!")
+      ],
+    );
+  }
+}
