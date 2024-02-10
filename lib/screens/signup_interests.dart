0482f507f6e693ceb722913ea7a2c2a6a483987a
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-02-10 09:44:26 +0530

    changed to stateful widget

--- lib/screens/signup_interests.dart
+++ lib/screens/signup_interests.dart
@@ -1,47 +1,51 @@
 import 'package:flutter/material.dart';
 
-class SignUp_Interests extends StatelessWidget {
+class SignUp_Interests extends StatefulWidget {
   const SignUp_Interests({super.key});
 
   @override
+  State<SignUp_Interests> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<SignUp_Interests> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          // logo  
-              Image.asset('assets/logo.png'),
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // logo
+          Image.asset('assets/logo.png'),
 
-              const SizedBox(height: 56),
+          const SizedBox(height: 56),
           // title
-              const Text(
-                "Choose your interests",
-                style: TextStyle(
-                  fontFamily: "Poppins",
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
+          const Text(
+            "Choose your interests",
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
 
-              const SizedBox(height: 11),
+          const SizedBox(height: 11),
           // text
-              const Text(
-                "Please choose your interests so we can recommend more relevant therapy suggestions  ",
-                style: TextStyle(
-                  fontFamily: "Poppins",
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+          const Text(
+            "Please choose your interests so we can recommend more relevant therapy suggestions  ",
+            style: TextStyle(
+              fontFamily: "Poppins",
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
 
+          const SizedBox(height: 30),
           // checkboxes
 
           // previous and next buttons
-          ],
-        ),
-      )
-
-    );
+        ],
+      ),
+    ));
   }
 }
