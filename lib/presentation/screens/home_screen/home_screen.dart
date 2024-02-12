be0dc084728200dfcb606cc19fb07ee3756bb37c
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-02-13 02:17:30 +0530

    placeholder / botton nav bar added

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soulnest/presentation/screens/home_screen/widgets/home_active_minutes.dart';
 import 'package:soulnest/presentation/screens/home_screen/widgets/home_header.dart';
 import 'package:soulnest/presentation/screens/home_screen/widgets/home_recent_activites.dart';
 
@@ -11,11 +12,28 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.man_3_sharp),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: const Column(
         children: [
           HomeHeader(),
           RecentActivities(),
+          ActiveMinutes(),
         ],
       ),
     );
