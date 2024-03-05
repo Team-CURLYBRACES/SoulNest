458559793057ce0ea9271ee2184067c03ef375b7
amethaaa <amethajayawardena@gmail.com>
2024-03-05 11:55:16 +0530

    add chat screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: ChatPage(),
+));
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
